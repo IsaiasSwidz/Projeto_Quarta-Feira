b0f158a085228ad4fdf531c8e83e31456b0183bd
Lucas <105462862+lucasalvisimader@users.noreply.github.com>
2025-06-12 11:40:23 -0300

    página de login criada

--- js/AccountCreation.js
+++ js/AccountCreation.js
@@ -1,8 +1,8 @@
 document.getElementById('btn-anexar').addEventListener('click', function() {
-    document.getElementById('documento').click();
+    document.getElementById('input-file').click();
 });
 
-document.getElementById('documento').addEventListener('change', function() {
+document.getElementById('input-file').addEventListener('change', function() {
     const fileName = this.files[0] ? this.files[0].name : '';
     document.getElementById('file-chosen').textContent = fileName;
 });
